557a74d2132c086591434c8c962e7d5a0ae3142e
ayushnishad <ayushnishadcr7@gmail.com>
2021-05-31 08:02:38 +0530

    Assignment 3: initial commit.

--- src/test/java/Assignment2/StudentServiceTest.java
+++ src/test/java/Assignment2/StudentServiceTest.java
@@ -1,17 +1,20 @@
 package Assignment2;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class StudentServiceTest {
+
   StudentService studentService;
+
   @BeforeEach
-  void intializeService()
-  {
+  void intializeService() {
     studentService = new StudentService();
   }
+
   @Test
   void checkIfNameEmpty() {
     assertEquals(false, studentService.validInput("", "12", "abs scs", "12", "A B C D"));
@@ -25,71 +28,61 @@ class StudentServiceTest {
 
   @Test
   void checkIfAddressEmpty() {
-
     assertEquals(false, studentService.validInput("abc", "12", "", "12", "A B C D"));
   }
 
   @Test
   void checkIfAgeEmpty() {
-
     assertEquals(false, studentService.validInput("abc", "12", "abs scs", "", "A B C D"));
   }
+
   @Test
   void checkIfAgeZero() {
-
     assertEquals(false, studentService.validInput("abc", "12", "abs scs", "0", "A B C D"));
   }
 
 
   @Test
   void checkIfCourseEmpty() {
-
     assertEquals(false, studentService.validInput("abc", "12", "abs scs", "12", ""));
   }
 
   @Test
   @DisplayName("When name format is wrong")
   void checkInputName() {
-
     assertEquals(false, studentService.validInput("abc2", "12", "x y z", "21", "A B C D"));
   }
 
   @Test
   @DisplayName("When rollNo format is wrong")
   void checkInputRollNo() {
-
     assertEquals(false, studentService.validInput("abc", "2F", "x y z", "21", "A B C D"));
   }
 
   @Test
   @DisplayName("When Age format is wrong")
   void checkInputAge() {
-
     assertEquals(false, studentService.validInput("abc", "12", "x y z", "2A", "A B C D"));
   }
 
   @Test
   @DisplayName("When Course format is wrong")
   void checkInputCourse() {
-
     assertEquals(false, studentService.validInput("abc", "12", "x y z", "21", "A B C 1 2 34"));
   }
 
   @Test
   void checkIfRepeatCourse() {
-
     assertEquals(false, studentService.validInput("abc", "12", "abs scs", "12", "A A C D"));
   }
 
   @Test
   void checkIfAtleast4Course() {
-
     assertEquals(false, studentService.validInput("abc", "12", "abs scs", "12", "A C D"));
   }
 
   @Test
   void checkIfRollNoAlreadyExistWhenAddingStudent() {
-
     studentService.processInput("abc", "12", "x y z", "12", "A B C D");
     studentService.processInput("def", "12", "m n o", "22", "C D E F");
     assertEquals(1, studentService.studentList.size());
@@ -112,13 +105,13 @@ class StudentServiceTest {
     assertEquals(21, studentService.studentList.get(0).age);
     assertEquals(12, studentService.studentList.get(0).rollNo);
     assertArrayEquals(new StudentService.courseOption[]{StudentService.courseOption.A,
-        StudentService.courseOption.B, StudentService.courseOption.C}, studentService.studentList.get(0).courses);
+            StudentService.courseOption.B, StudentService.courseOption.C},
+        studentService.studentList.get(0).courses);
     assertEquals(true, studentService.isStateChanged);
   }
 
   @Test
   void checkSortStudentListNameAscending() {
-
     studentService.processInput("def", "13", "x y z", "13", "B C D E");
     studentService.processInput("abc", "12", "x y z", "21", "A B C D");
     studentService.currentType = StudentService.sortOption.NAME;
@@ -129,7 +122,6 @@ class StudentServiceTest {
 
   @Test
   void checkSortStudentListNameDescending() {
-
     studentService.processInput("def", "13", "x y z", "13", "B C D E");
     studentService.processInput("abc", "12", "x y z", "21", "A B C D");
     studentService.currentType = StudentService.sortOption.NAME;
@@ -140,7 +132,6 @@ class StudentServiceTest {
 
   @Test
   void checkSortStudentListAgeAscending() {
-
     studentService.processInput("def", "13", "x y z", "13", "B C D E");
     studentService.processInput("abc", "12", "x y z", "21", "A B C D");
     studentService.currentType = StudentService.sortOption.AGE;
@@ -151,7 +142,6 @@ class StudentServiceTest {
 
   @Test
   void checkSortStudentListAgeDescending() {
-
     studentService.processInput("def", "13", "x y z", "13", "B C D E");
     studentService.processInput("abc", "12", "x y z", "21", "A B C D");
     studentService.currentType = StudentService.sortOption.AGE;
@@ -162,7 +152,6 @@ class StudentServiceTest {
 
   @Test
   void checkSortStudentListRollNoAscending() {
-
     studentService.processInput("def", "13", "x y z", "13", "B C D E");
     studentService.processInput("abc", "12", "x y z", "21", "A B C D");
     studentService.currentType = StudentService.sortOption.ROLL_NO;
@@ -173,7 +162,6 @@ class StudentServiceTest {
 
   @Test
   void checkSortStudentListRollNoDescending() {
-
     studentService.processInput("def", "13", "x y z", "13", "B C D E");
     studentService.processInput("abc", "12", "x y z", "21", "A B C D");
     studentService.currentType = StudentService.sortOption.ROLL_NO;
@@ -184,7 +172,6 @@ class StudentServiceTest {
 
   @Test
   void checkShowData() {
-
     studentService.processInput("def", "13", "x y z", "13", "B C D E");
     studentService.processInput("abc", "12", "x y z", "21", "A B C D");
     studentService.currentType = StudentService.sortOption.ROLL_NO;
@@ -195,14 +182,12 @@ class StudentServiceTest {
 
   @Test
   void checkSortPreference() {
-
     studentService.sortPreference(StudentService.sortOption.ROLL_NO, true);
     assertEquals(true, studentService.isStateChanged);
   }
 
   @Test
   void checkDeleteStudent() {
-
     studentService.processInput("abc", "12", "x y z", "21", "A B C D");
     studentService.processInput("def", "13", "x y z", "13", "B C D E");
     studentService.deleteStudent("13");
@@ -213,7 +198,6 @@ class StudentServiceTest {
 
   @Test
   void checkDeleteStudentWhenRollNoEmpty() {
-
     studentService.processInput("abc", "12", "x y z", "21", "A B C D");
     studentService.processInput("def", "13", "x y z", "13", "B C D E");
     studentService.deleteStudent("");
@@ -222,7 +206,6 @@ class StudentServiceTest {
 
   @Test
   void checkDeleteStudentWhenRollFormatIsWrong() {
-
     studentService.processInput("abc", "12", "x y z", "21", "A B C D");
     studentService.processInput("def", "13", "x y z", "13", "B C D E");
     studentService.deleteStudent("3ds");
@@ -231,7 +214,6 @@ class StudentServiceTest {
 
   @Test
   void checkDeleteStudentWhenRollNoIsIncorrect() {
-
     studentService.processInput("abc", "12", "x y z", "21", "A B C D");
     studentService.processInput("def", "13", "x y z", "13", "B C D E");
     studentService.deleteStudent("35");
